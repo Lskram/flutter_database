a12d46532fcca2735953557bb44ea0a1c272ff81
Lskram <dankflamemasterLove@gmail.com>
2025-07-02 22:57:16 +0700

    การตั้งค่าฟอร์มยิบย่อย

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -7,7 +7,40 @@ class FormScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("แบบฟอร์มบันทึกข้อมูล")),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "ชื่อรายการ",
+                  border: OutlineInputBorder(),
+                ),
+                autofocus: true,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "จำนวนเงิน",
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text("เพิ่มข้อมูล"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
